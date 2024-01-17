74973c8f1e883f4a01497d13966c357a791a36c8
r6732 <155535117+r6732@users.noreply.github.com>
2024-01-17 12:05:43 +0100

    Add files via upload

---
+++ Karte_Interface.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+public interface KarteInterface {
+
+public string obersteKarteZiehen ();
+
+public void KartenVerschieben ();
+
+{
+Collections.shuffle ();
+}
+}
