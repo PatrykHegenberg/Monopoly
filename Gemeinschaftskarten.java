06e14d9259fdfb3d7575c23e36a872a3e5eb6e20
r6732 <155535117+r6732@users.noreply.github.com>
2024-01-23 13:17:11 +0100

    Update Gemeinschaftskarten.java

--- Gemeinschaftskarten.java
+++ Gemeinschaftskarten.java
@@ -8,38 +8,38 @@ public class GemeinschaftskartenKlasse implements Karten {
 
 //Hinzufuegen der Gemeinschaftskarten
 {
-    Gemeinschaftskarten.add("Sie kommen aus dem Gefängnis frei! Behalten Sie diese Karte, bis Sie sie benötigen oder verkaufen.");
+    Gemeinschaftskarten.add("Sie kommen aus dem Gefaengnis frei! Behalten Sie diese Karte, bis Sie sie benoetigen oder verkaufen.");
     Gemeinschaftskarten.add("Schulgeld. Zahlen Sie 50 Euro");
     Gemeinschaftskarten.add("Urlaubsgeld! Sie erhalten 100 Euro.");
-    Gemeinschaftskarten.add("Ihre Lebensversicherung wird fällig. Sie erhalten 100 Euro.");
+    Gemeinschaftskarten.add("Ihre Lebensversicherung wird faellig. Sie erhalten 100 Euro.");
     Gemeinschaftskarten.add("Arzt-Kosten. Zahlen Sie 50 Euro.");
-    Gemeinschaftskarten.add("Einkommenssteuerrückerstattung. Sie erhalten 20 Euro");
-    Gemeinschaftskarten.add("Krankenhausgebühren. Zahlen Sie 100 Euro.");
-    Gemeinschaftskarten.add("Gehen Sie in das Gefängnis. Begeben Sie sich direkt dorthin. Gehen Sie nicht über Los. Ziehen Sie nicht 200 Euro ein.");
+    Gemeinschaftskarten.add("Einkommenssteuerrueckerstattung. Sie erhalten 20 Euro");
+    Gemeinschaftskarten.add("Krankenhausgebuehren. Zahlen Sie 100 Euro.");
+    Gemeinschaftskarten.add("Gehen Sie in das Gefaengnis. Begeben Sie sich direkt dorthin. Gehen Sie nicht ueber Los. Ziehen Sie nicht 200 Euro ein.");
     Gemeinschaftskarten.add("Sie erhalten auf Vorzugs-Aktien 7% Dividende: 25 Euro.");
     Gemeinschaftskarten.add("Sie haben Geburtstag. Jeder Spieler schenkt Ihnen 10 Euro.");
     Gemeinschaftskarten.add("Sie erben 100 Euro.");
-    Gemeinschaftskarten.add("Aus Lagerverkäufen erhalten Sie 50 Euro.");
-    Gemeinschaftskarten.add("Zweiter Preis im Schönheitswettbewerb. Sie erhalten 10 Euro.");
-    Gemeinschaftskarten.add("Sie werden zu Straßenausbesserungsarbeiten herangezogen. Zahlen Sie 40 Euro je Haus und 115 Euro je Hotel an die Bank.");
-    Gemeinschaftskarten.add("Rücken Sie vor bis auf Los. (Ziehe 200 Euro ein).");
+    Gemeinschaftskarten.add("Aus Lagerverkaeufen erhalten Sie 50 Euro.");
+    Gemeinschaftskarten.add("Zweiter Preis im Schoenheitswettbewerb. Sie erhalten 10 Euro.");
+    Gemeinschaftskarten.add("Sie werden zu Strassenausbesserungsarbeiten herangezogen. Zahlen Sie 40 Euro je Haus und 115 Euro je Hotel an die Bank.");
+    Gemeinschaftskarten.add("Ruecken Sie vor bis auf Los. (Ziehe 200 Euro ein).");
     Gemeinschaftskarten.add("Bank-Irrtum zu Ihren Gunsten. Ziehen Sie 200 Euro ein");
 }
 
 //Methoden
 
 public string obersteKarteZiehen () {
-    string obersteKarteZiehen = Ereigniskarten.get(0);
-
-    System.out.println("Sie haben folgende Karte gezogen:" + obersteKarteZiehen);
+    string obersteKarte = Gemeinschaftskarten.get(0);
+    return obersteKarte;
+    System.out.println("Sie haben folgende Karte gezogen:" + obersteKarte);
 }
 
 public void KartenVerschieben () {
-    string obersteKarte = Ereigniskarten.removeFirst(0);
-    Ereigniskarten.addLast (obersteKarte);
+    string obersteKarte = Gemeinschaftskarten.removeFirst(0);
+    Gemeinschaftskarten.addLast (obersteKarte);
 }
 {
-    Collections.shuffle (Gemeinschaftskarten);
-    System.out.println("Der Stapel wurde gemischt:" + Gemeinschaftskarten);
-    }
+Collections.shuffle (Gemeinschaftskarten);
+System.out.println("Der Stapel wurde neu gemischt.");
+}
 }
