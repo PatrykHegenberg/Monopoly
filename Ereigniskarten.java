45a49248231e33ba0b8fc7f65a31247dbf010fc6
r6732 <155535117+r6732@users.noreply.github.com>
2024-01-23 13:16:54 +0100

    Update Ereigniskarten.java

--- Ereigniskarten.java
+++ Ereigniskarten.java
@@ -8,33 +8,33 @@ public class EreigniskartenKlasse implements Karten {
 
 //Hinzufuegen der Ereigniskarten
 {
-    Ereigniskarten.add("Rücken Sie vor bis zur Schlossallee.");
-    Ereigniskarten.add("Machen Sie einen Ausflug zum Südbahnhof. Wenn Sie über Los kommen, ziehen Sie 200 Euro ein");
-    Ereigniskarten.add("Ihr Bausparvertrag wird fällig. Sie erhalten 200 Euro.");
-    Ereigniskarten.add("Rücken Sie vor bis zum Opernplatz. Wenn Sie über Los kommen, ziehen Sie 200 Euro ein.");
-    Ereigniskarten.add("Rücken Sie vor bis zum nächsten Versorgungswerk." + 
-    "Werfen Sie die Würfel und zahlen dem Eigentümer den zehnfachen Betrag Ihres Wurfergebnisses." + 
-    "Wenn das Werk noch niemandem gehört, können Sie es von der Bank kaufen.");
-    Ereigniskarten.add("Gehen Sie in das Gefängnis. Begeben Sie sich direkt dorthin. Gehen Sie nicht über Los. Ziehen Sie nicht 200 Euro ein.");
-    Ereigniskarten.add("Rücken Sie vor bis auf Los. (Ziehe 200 Euro ein).");
+    Ereigniskarten.add("Ruecken Sie vor bis zur Schlossallee.");
+    Ereigniskarten.add("Machen Sie einen Ausflug zum Suedbahnhof. Wenn Sie ueber Los kommen, ziehen Sie 200 Euro ein");
+    Ereigniskarten.add("Ihr Bausparvertrag wird faellig. Sie erhalten 200 Euro.");
+    Ereigniskarten.add("Ruecken Sie vor bis zum Opernplatz. Wenn Sie ueber Los kommen, ziehen Sie 200 Euro ein.");
+    Ereigniskarten.add("Ruecken Sie vor bis zum naechsten Versorgungswerk." + 
+    "Werfen Sie die Wuerfel und zahlen dem Eigentuemer den zehnfachen Betrag Ihres Wurfergebnisses." + 
+    "Wenn das Werk noch niemandem gehoert, koennen Sie es von der Bank kaufen.");
+    Ereigniskarten.add("Gehen Sie in das Gefaengnis. Begeben Sie sich direkt dorthin. Gehen Sie nicht ueber Los. Ziehen Sie nicht 200 Euro ein.");
+    Ereigniskarten.add("Ruecken Sie vor bis auf Los. (Ziehe 200 Euro ein).");
     Ereigniskarten.add("Die Bank zahlt Ihnen eine Dividende von 50 Euro.");
-    Ereigniskarten.add("Sie lassen Ihre Häuser renovieren. Zahlen Sie: 25 Euro pro Haus, 100 Euro pro Hotel.");
-    Ereigniskarten.add("Sie kommen aus dem Gefängnis frei! Behalten Sie diese Karte, bis Sie sie benötigen oder verkaufen");
-    Ereigniskarten.add("Rücken Sie vor bis zur Seestraße. Wenn Sie über Los kommen, ziehen Sie 200 Euro ein.");
-    Ereigniskarten.add("Sie sind zum Vorstand gewählt worden. Zahlen Sie jedem Spieler 50 Euro.");
-    Ereigniskarten.add("Gehen Sie 3 Felder zurück.");
+    Ereigniskarten.add("Sie lassen Ihre Haeuser renovieren. Zahlen Sie: 25 Euro pro Haus, 100 Euro pro Hotel.");
+    Ereigniskarten.add("Sie kommen aus dem Gefaengnis frei! Behalten Sie diese Karte, bis Sie sie benoetigen oder verkaufen");
+    Ereigniskarten.add("Ruecken Sie vor bis zur Seestrasse. Wenn Sie ueber Los kommen, ziehen Sie 200 Euro ein.");
+    Ereigniskarten.add("Sie sind zum Vorstand gewaehlt worden. Zahlen Sie jedem Spieler 50 Euro.");
+    Ereigniskarten.add("Gehen Sie 3 Felder zurueck.");
     Ereigniskarten.add("Strafzettel! Zahlen Sie 15 Euro.");
-    Ereigniskarten.add("Rücken Sie vor bis zum nächsten Verkehrsfeld." + 
-    "Der Eigentümer erhält das Doppelte der normalen Miete." +
-    "Wenn das Verkehrsfeld noch niemandem gehört, können Sie es von der Bank kaufen.");
+    Ereigniskarten.add("Ruecken Sie vor bis zum naechsten Verkehrsfeld." + 
+    "Der Eigentuemer erhaelt das Doppelte der normalen Miete." +
+    "Wenn das Verkehrsfeld noch niemandem gehoert, koennen Sie es von der Bank kaufen.");
 }
 
 //Methoden
 
 public string obersteKarteZiehen () {
-    string obersteKarteZiehen = Ereigniskarten.get(0);
-
-    System.out.println("Sie haben folgende Karte gezogen:" + obersteKarteZiehen);
+    string obersteKarte = Ereigniskarten.get(0);
+    return obersteKarte;
+    System.out.println("Sie haben folgende Karte gezogen:" + obersteKarte);
 }
 
 public void KartenVerschieben () {
@@ -44,6 +44,6 @@ public void KartenVerschieben () {
 
 {
 Collections.shuffle (Ereigniskarten);
-System.out.println("Der Stapel wurde gemischt:" + Ereigniskarten);
+System.out.println("Der Stapel wurde neu gemischt.");
 }
 }
