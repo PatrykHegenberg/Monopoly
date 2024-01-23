fc44894caa5f85bb431c30e81a7b8decb212b6d7
r6732 <155535117+r6732@users.noreply.github.com>
2024-01-23 13:39:30 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
+                
 		Controller myController = new Controller();
 		View myView = new View();
 		Model myModel = new Model();
@@ -14,5 +20,38 @@ public class main {
 		myView.init();
 		
 	}
+{
+        // JDBC-Verbindungsparameter
+        String jdbcUrl = "jdbc:mysql://localhost:3306/deineDatenbank";
+        String benutzername = "deinBenutzername";
+        String passwort = "deinPasswort";
 
+        // SQL-Abfrage
+        String sqlAbfrage = "SELECT * FROM deineTabelle";
+
+        try {
+            // Verbindung zur Datenbank herstellen
+            Connection verbindung = DriverManager.getConnection(jdbcUrl, benutzername, passwort);
+
+            // SQL-Statement erstellen
+            Statement statement = verbindung.createStatement();
+
+            // SQL-Abfrage ausführen
+            ResultSet ergebnis = statement.executeQuery(sqlAbfrage);
+
+            // Ergebnisse verarbeiten
+            while (ergebnis.next()) {
+                // Beispiel: Ausgabe von Spaltenwerten
+                int id = ergebnis.getInt("id");
+                String name = ergebnis.getString("name");
+                System.out.println("ID: " + id + ", Name: " + name);
+            }
+
+            // Verbindung schließen
+            verbindung.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+}
 }
