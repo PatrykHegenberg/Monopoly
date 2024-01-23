36ea44067fc6c3e19882d241e084aa1a6cd1481c
r6732 <155535117+r6732@users.noreply.github.com>
2024-01-23 16:29:14 +0100

    Add files via upload

---
+++ Spielfeld_AbstrakteKlasse.java
@@ -0,0 +1,18 @@
+abstract class Spielfeld {
+
+//Attribute
+
+public int ID;
+public String Name;
+
+//Methoden
+
+public abstract SpielerHinzufuegen ();
+
+public abstract StrasseKaufen ();
+
+public abstract BahnhofKaufen ();
+
+public abstract WerkKaufen ();
+
+}
